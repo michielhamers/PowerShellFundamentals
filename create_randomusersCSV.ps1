de46aeaccc0be4d1f413be767b974b9ed3b9d1ef
Michiel Hamers (Hammertime) <michielhamers@outlook.com>
2022-01-20 08:35:20 +0100

    Add files via upload

---
+++ create_randomusersCSV.ps1
@@ -0,0 +1,6 @@
+﻿#Set-ExecutionPolicy -ExecutionPolicy
+$RandomusersetCSV = Invoke-RestMethod "https://randomuser.me/api?inc=name,nat,email&results=3&format=csv"
+$Randomuserset = $RandomusersetCSV | ConvertFrom-Csv 
+$Randomuserset | Export-Csv -LiteralPath C:\code\PS1demo\users.csv -NoTypeInformation 
+$Randomuserset
+$Randomusersetcsv
